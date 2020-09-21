01a8804d8093bd3473a3f0cf883bbd4aeccb4da4
Mike Fulton <mikefultonpersonal@gmail.com>
2020-09-21 15:55:54 -0400

    working python example for z/OS or non-z/OS (tested on Mac)

--- bin/xsysvar.py
+++ bin/xsysvar.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 
-my_header = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate',
-    'Referer': 'https://127.0.0.1', 
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Host':None
+header = {
+    'X-CSRF-ZOSMF-HEADER': 'zosmf',
 }
 
 import requests
-#URL you want to access
-geturl ="https://127.0.0.1:10443/zosmf/variables/rest/1.0/systems/ADCDPL.S0W1"
+url ="https://127.0.0.1:10443/zosmf/variables/rest/1.0/systems/ADCDPL.S0W1"
+url ="https://torzdt1.canlab.ibm.com:8136/zosmf/variables/rest/1.0/systems/ADCDPL.S0W1"
 
-#By default, you start to disable SSL verification
-res = requests.get(geturl,cert=('/tmp/zosmf-crt.pem', '/tmp/zosmf-key.pem'),headers=my_header,verify=False)
+res = requests.get(url,cert=('/u/ibmuser/certs/zosmf-crt.pem', '/u/ibmuser/certs/zosmf-key.pem'),headers=header,verify=False)
 
 print(res.status_code)
 print(res.text)
-
