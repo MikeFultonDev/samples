699de345eff2b937f6b7962e8bd15a0b0c45e372
Mike Fulton <mikefultonpersonal@gmail.com>
2021-05-29 00:33:43 -0400

    incorporate review comments from Onno

--- src/call31a.s
+++ src/call31a.s
@@ -10,28 +10,32 @@ CALL31A RMODE      ANY
 *         ---------------                                             *
 *           @parms        ->  31-bit parameter list for target routine*
 *         ---------------
-         LG         15,0(,1)       # target fn pointer
-         LGR        0,13           # save R13 into R0
-         LG         13,8(,1)       # load 31-bit DSA into R13
-         LG         1,16(,1)       # load 31-bit R1 into R1
-         STG        0,0(,13)       # store old R13 (dsa)
-         STG        14,8(,13)      # store old R14 (return address)
-         LA         13,16(,13)     # move 31-bit DSA to user DSA
-         ST         13,76(,13)     # store NAB of DSA
+         LG         R15,0(,R1)       # target fn pointer
+         LGR        R0,R13           # save R13 into R0
+         LG         R13,8(,R1)       # load 31-bit DSA into R13
+         LG         R1,16(,R1)       # load 31-bit R1 into R1
+         STG        R0,0(,R13)       # store old R13 (dsa)
+         STG        R14,8(,R13)      # store old R14 (return address)
+         LA         R13,16(,R13)     # move 31-bit DSA to user DSA
+         ST         R13,76(,R13)     # store NAB of DSA
 *
-         OILH       15,X'8000'     # set bit 32 on in target fn
-         BASSM      14,15          # branch-and-set to 31-bit mode
+         OILH       R15,X'8000'      # set bit 32 on in target fn
+         BASSM      R14,R15          # branch-and-set to 31-bit mode
 *
 * Reload 64-bit R13 from start of low memory storage area
 *
 
 
-         AHI        13,-16
-         LG         14,8(,13)      # restore R14 return address
-         LG         13,0(,13)
+         AHI        R13,-16
+         LG         R14,8(,R13)      # restore R14 return address
+         LG         R13,0(,R13)
 *
 * Abbreviated epilog
 *
-         BR         14
+         BR         R14
+         LTORG
+         DROP
+*
+         YREGS
 *
          END
